a94037e71b962ff47eaa82c1b00de5278c4529b6
Elias-ReactDeveloper <eliasvasques74@gmail.com>
2023-04-25 23:26:12 -0300

    feat: cria componente Feed

---
+++ src/components/ Feed/Feed.tsx
@@ -0,0 +1,32 @@
+import Card from "../Card";
+import { StaticImageData } from "next/image";
+
+import strangerThings from "../../../public/assets/stranger-things.jpg";
+import agenteNoturno from "../../../public/assets/agente-noturno.jpg";
+import suits from "../../../public/assets/suits.webp";
+import onePiece from "../../../public/assets/one-piece.webp";
+import run from "../../../public/assets/run.webp";
+
+const imagesSrc: StaticImageData[] = [
+  strangerThings,
+  agenteNoturno,
+  suits,
+  onePiece,
+  run,
+  strangerThings,
+];
+
+const Feed = () => {
+  return (
+    <div className="feed">
+      <h3 className="feed__title">Continuar assistindo como Nany</h3>
+      <div className="feed__cards">
+        {imagesSrc.map((src) => (
+          <Card src={src} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
